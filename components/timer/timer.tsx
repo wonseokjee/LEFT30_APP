adcab87b68b19bdc3d669b03f8a379e35e2371f5
wonseokjee <wsji9404@gmail.com>
2024-12-08 12:14:53 +0900

    [Feat]timer 기능 추가

---
+++ components/timer/timer.tsx
@@ -0,0 +1,21 @@
+import useInterval from '@/hooks/timer/useInterval';
+import useTimer from '@/hooks/timer/useTimer';
+import { Text, View } from 'react-native';
+
+export default function Timer() {
+  const { time, leftMinute, leftSecond } = useTimer();
+  useInterval(() => {
+    if (leftSecond == '00') {
+      console.log('dddddd');
+      
+    }
+  }, 1000);
+  return (
+    <View>
+      <Text>{time}</Text>
+      <Text>
+        {leftMinute}:{leftSecond}
+      </Text>
+    </View>
+  );
+}
