acaf265910b556837f8c5eadaf1361d1a7186490
wonseokjee <wsji9404@gmail.com>
2024-12-26 21:53:37 +0900

    [Feat]modal 파일에 firebase 입력 api 적용

--- hooks/alarm/modal.tsx
+++ hooks/alarm/modal.tsx
@@ -11,6 +11,8 @@ import {
   TouchableOpacity,
 } from 'react-native';
 import CheckBox from './checkbox';
+import { setFirebaseCollection_Test } from '@/api/firebase';
+import { setCollection_Test } from '@/@types/firebase/collections';
 
 export interface checkProps {
   checkValue: (str: string) => void;
@@ -27,20 +29,29 @@ const ModalButton = () => {
   };
 
   const handleConfirm = async () => {
+    //checkbox가 체크되어 있지 않으면 confirm 안되게
     // console.log('입력된 값:', inputValue);
-    //action이 null 값인 경우 경고 or 버튼 안눌리게
-    const date = new Date().toString();
-    await setDoc(doc(DB, 'test', date), {
-      action: checkboxValue,
-      detail: inputValue,
-      date: date,
-    });
-    setModalVisible(false);
+    if (checkboxValue) {
+      //firebase collection 'test'에 들어갈 value
+      const testValue: setCollection_Test = {
+        date: Date().toString(),
+        action: checkboxValue,
+        detail: inputValue,
+      };
+      //firebase에 입력
+      setFirebaseCollection_Test('test', testValue);
+      setModalVisible(false);
+    } else {
+      //확인버튼 눌렀을때 checkbox 체크 안되어 있으면 '한 일을 체크해주세요' 문구가 떨리는 effect추가하기
+    }
     setCheckboxValue(null);
   };
 
+  //취소하면 이전 행동 지속하게 or 이전행동 지속 버튼 만들기??
   const handleCancel = () => {
     setModalVisible(false);
+    setCheckboxValue(null);
+    setInputValue('');
   };
 
   return (
@@ -54,11 +65,12 @@ const ModalButton = () => {
       >
         <View style={styles.overlay}>
           <View style={styles.alertBox}>
-            <Text style={styles.alertTitle}>입력 요청</Text>
+            <Text style={styles.alertTitle}>어떤 일을 하셨나요?</Text>
             <CheckBox checkValue={checkboxHandler} />
+            {checkboxValue ? <></> : <Text>한 일을 체크 해주세요!</Text>}
             <TextInput
               style={styles.input}
-              placeholder='여기에 입력하세요'
+              placeholder='세부계획을 입력해주세요'
               value={inputValue}
               onChangeText={setInputValue}
             />
