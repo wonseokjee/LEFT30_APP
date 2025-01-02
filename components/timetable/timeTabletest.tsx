4ad330ec131cf2412b021f9a34255b0282155b7b
wonseokjee <wsji9404@gmail.com>
2025-01-02 22:20:30 +0900

    [Comment] timeSlot 타입 지정정

--- components/timetable/timeTabletest.tsx
+++ components/timetable/timeTabletest.tsx
@@ -6,10 +6,21 @@ import {
   ScrollView,
   TouchableOpacity,
 } from 'react-native';
+type TimeSlot = {
+  startTime: string;
+  endTime: string;
+  block: string;
+  detail: string;
+  isContinue: boolean; //이전행동 지속하기
+};
+
 
 const timeTableTest = () => {
+  //notion calendar와 비슷한 양식으로
+  //일단 빠르게 구현하기. 최적화는 나중에. 규모 커지면 어떻게해서든 문제 생긴다. 
   return (
     <ScrollView>
+      
       <Text>ㅇㅇ</Text>
     </ScrollView>
   );
