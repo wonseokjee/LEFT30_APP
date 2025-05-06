d40f8916e59df1b6c9b692fb3429ce6bba8907a5
wonseokjee <wsji9404@gmail.com>
2025-05-06 23:17:31 +0900

    [Refactor] useTimeSlotInfo with zustand

--- application/store/timeTableStore.tsx
+++ application/store/timeTableStore.tsx
@@ -8,6 +8,14 @@ interface useTimeSlotStore_yesterday {
   yesterdaydata: { [x: string]: any }[] | null;
   setTimeSlot: (yesterdaydata: { [x: string]: any }[]) => void;
 }
+interface useTimeSlotStore_today {
+  todaydata: { [x: string]: any }[] | null;
+  setTimeSlot: (todaydata: { [x: string]: any }[]) => void;
+}
+interface useTimeSlotStore_yesterday {
+  yesterdaydata: { [x: string]: any }[] | null;
+  setTimeSlot: (yesterdaydata: { [x: string]: any }[]) => void;
+}
 
 export const useTimeSlotStore_today = create<useTimeSlotStore_today>((set) => ({
   todaydata: null,
@@ -16,6 +24,17 @@ export const useTimeSlotStore_today = create<useTimeSlotStore_today>((set) => ({
       todaydata: data,
     })),
 }));
+
+export const useTodayTimeSlotInfoFromZustand = create<useTimeSlotStore_today>(
+  (set) => ({
+    todaydata: null,
+    setTimeSlot: (data: { [x: string]: any }[]) =>
+      set(() => ({
+        todaydata: data,
+      })),
+  })
+);
+
 export const useTimeSlotStore_yesterday = create<useTimeSlotStore_yesterday>(
   (set) => ({
     yesterdaydata: null,
@@ -25,5 +44,14 @@ export const useTimeSlotStore_yesterday = create<useTimeSlotStore_yesterday>(
       })),
   })
 );
+
+export const useYesterdayTimeSlotInfoFromZustand =
+  create<useTimeSlotStore_yesterday>((set) => ({
+    yesterdaydata: null,
+    setTimeSlot: (data: { [x: string]: any }[]) =>
+      set(() => ({
+        yesterdaydata: data,
+      })),
+  }));
 
 // export default { useTimeSlotStore_today, useTimeSlotStore_yesterday };
