5a8fcab6c7cc1e94dc78e45c5ef36a4aadc25479
wonseokjee <wsji9404@gmail.com>
2025-09-11 15:45:42 +0900

    [design] palette - gray0~9 색상 추가

---
+++ application/assets/palette.tsx
@@ -0,0 +1,22 @@
+export const GRAY_0 = '#f8f9fa'; //배경
+export const GRAY_1 = '#f1f3f5'; // 배경
+export const GRAY_2 = '#e9ecef'; // 비활성상태
+export const GRAY_3 = '#dee2e6'; // 선요소, 요소간 구획
+export const GRAY_4 = '#ced4da'; // 폼이나 버튼의 테두리, 동작할수있는 선 암시
+
+export const GRAY_5 = '#adb5bd'; // 텍스트
+//비활성임을 나타내는 텍스트
+
+export const GRAY_6 = '#868e96'; // 텍스트
+//본문 텍스트보다는 옅지만, 비활성으로 느껴질 만큼 옅지 않기 때문에 사용자에게 중요하지 않은 정보, 혹은 부연 설명 정도로 인식
+
+export const GRAY_7 = '#495057'; // 텍스트
+//부드러운 느낌이 나며, 배경과 자연스럽게 어우러진다. 메뉴 레이블, 버튼 텍스트 등 기능적인 UI 요소에 적절
+
+export const GRAY_8 = '#343a40'; // 텍스트
+//블로그나 SNS, 헤드라인 등 콘텐츠가 강조되는 곳에서는 글씨가 배경과 또렷이 구분되는 8번이 적합
+
+export const GRAY_9 = '#212529';
+
+//반전색상
+//배경은 gray 9, 텍스트는 상태에 따라 gray 0, gray 5, gray 6, 선 요소는 gray 7을 쓴다.
