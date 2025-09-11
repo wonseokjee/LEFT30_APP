7fdb0fb20658340e80e8eba8780f6f7262d3558c
wonseokjee <wsji9404@gmail.com>
2025-09-11 15:47:26 +0900

    [Feat]GestureHandlerRootView 기능 추가

--- application/app/_layout.tsx
+++ application/app/_layout.tsx
@@ -12,6 +12,7 @@ import {
 import useNumStore from '@/store/timerStore';
 import registerForPushNotificationsAsync from '@/components/pushNotification/registerForPushNotificationsAsync';
 import { handleNotification } from '@/components/pushNotification/pushNotificationHandler';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 Notifications.setNotificationHandler({
   handleNotification: async (notification) => {
@@ -41,24 +42,26 @@ export default function RootLayout() {
   }, []);
   return (
     <>
-      <Stack>
-        <Stack.Screen
-          name='(login)/index' // Set the login page as the initial screen
-          options={{
-            headerShown: false,
-          }}
-        />
-        <Stack.Screen
-          name='(tabs)'
-          options={{
-            headerShown: false,
-            contentStyle: { backgroundColor: 'black' },
-          }}
-        />
-      </Stack>
-      {/* 어떤 화면에 있던지 trackerModal이 보일 수 있도록 layout에 위치함. */}
-      <ActionTrackerModal />
-      <StatusBar style='light' />
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <Stack>
+          <Stack.Screen
+            name='(login)/index' // Set the login page as the initial screen
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen
+            name='(tabs)'
+            options={{
+              headerShown: false,
+              contentStyle: { backgroundColor: 'black' },
+            }}
+          />
+        </Stack>
+        {/* 어떤 화면에 있던지 trackerModal이 보일 수 있도록 layout에 위치함. */}
+        <ActionTrackerModal />
+        <StatusBar style='light' />
+      </GestureHandlerRootView>
     </>
   );
 }
