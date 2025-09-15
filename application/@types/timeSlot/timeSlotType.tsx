5a34fd81e3f5a4dfc2a47f079b874d3b62af6988
wonseokjee <wsji9404@gmail.com>
2025-09-15 16:58:22 +0900

    [Type] type 수정 및 quietTimeType 지정

--- application/@types/timeSlot/timeSlotType.tsx
+++ application/@types/timeSlot/timeSlotType.tsx
@@ -1,16 +1,15 @@
 export type timeSlotType = {
   // userId: string;
-  id: string;
+  id?: string;
   action: string; // 일정 종류 (공부, 운동, 휴식 등)
-  category: string | null; 
-  color: string | null;
-  created_at: string;
+  category?: string | null;
+  color?: string | null;
+  created_at?: Date;
   description: string; // 내용
-  ended_at: string;
-  started_at: string;
-  tags: string | null;
+  ended_at: Date;
+  started_at?: Date;
+  tags?: string | null;
   range?: number | null; // 추가된 range 속성
 };
-
 
 export type timeSlotTypes = timeSlotType[];
