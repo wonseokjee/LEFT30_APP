49ad0a9dbc9c9553fe78b1b42abba3743521185c
wonseokjee <wsji9404@gmail.com>
2025-09-17 15:17:44 +0900

    [Feat]Home 화면에 명언 추가

--- application/app/(tabs)/index.tsx
+++ application/app/(tabs)/index.tsx
@@ -1,17 +1,19 @@
 import GetButton from '@/components/firebase/getTimeTable';
 import Timer from '@/components/timer/timer';
+import WiseSaying from '@/components/wiseSaying/wiseSaying';
 // import ModalButton from '@/hooks/alarm/modal';
 import useNumStore from '@/store/timerStore';
-import { StyleSheet, Button, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function Index() {
   const { setModalOpen } = useNumStore();
   return (
     <View style={styles.appContainer}>
       <Timer />
+      <WiseSaying />
       {/* <ModalButton /> */}
-      <Button title='Modal 열기' onPress={() => setModalOpen()} color='grey' />
-        <GetButton />
+      {/* <Button title='Modal 열기' onPress={() => setModalOpen()} color='grey' /> */}
+      {/* <GetButton /> */}
     </View>
   );
 }
@@ -21,5 +23,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: 'black',
+    // borderWidth: 1,
+    // borderColor: 'white',
   },
 });
